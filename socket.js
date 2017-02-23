7c9ec7ccb35582773e3630bbb755a0abfe973170
Nuno Ferro <nafferro@gmail.com>
2017-02-23 00:53:44 +0000

    source

--- socket.js
+++ socket.js
@@ -4,8 +4,8 @@ var host = 'xx.xx.xx.xx'; // Socket ip address
 var port = 8530;
 var macAddress = 'xxxxxxxxx'; // Socket mac address
 
-var on =  '104CF75F5A28A181574AC1B563CD51A78D'
-var off = '10F7B4E74B970D96F3CA2BB5D3CD1C19D0'
+var on =  '104CF75F5A28A181574AC1B563CD51A78D' //source https://forum.fhem.de/index.php?topic=38112.60
+var off = '10F7B4E74B970D96F3CA2BB5D3CD1C19D0' 
 
 var packet = '0142' + macAddress + eval(process.argv[2]);
 var buffer = new Buffer(packet, 'hex');
